fffc7b708bf9e0bad69484408f473d521b716bc5
Peter Bryant <peter@ptrbrynt.com>
2021-05-04 08:39:59 +0100

    ✨ Implement OAuthToken data class

---
+++ lib/src/oauth_token.dart
@@ -0,0 +1,34 @@
+/// An authentication token.
+class OAuthToken {
+  /// Constructs an [OAuthToken]
+  const OAuthToken({
+    required this.token,
+    required this.expiresAt,
+    required this.refreshToken,
+  });
+
+  /// The token used to authenticate requests.
+  ///
+  /// Should be added HTTP requests using a Bearer Authorization header.
+  final String token;
+
+  /// The [DateTime] at which this token expires.
+  final DateTime expiresAt;
+
+  /// A refresh token which can be used to generate a new token.
+  final String refreshToken;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is OAuthToken &&
+        other.token == token &&
+        other.expiresAt == expiresAt &&
+        other.refreshToken == refreshToken;
+  }
+
+  @override
+  int get hashCode =>
+      token.hashCode ^ expiresAt.hashCode ^ refreshToken.hashCode;
+}
